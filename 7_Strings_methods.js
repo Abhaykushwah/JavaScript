2c87434ed31edfae145ec20bcdcbc085e2cbf842
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-04-10 17:48:48 +0530

    Day 2 commite -- adding strings, mydate js files
    Here the files name 7_Strings_methods.js and 8_mydate.js uploaded

---
+++ 7_Strings_methods.js
@@ -0,0 +1,13 @@
+let mystr = "THis is MY String MY MY"
+console.log(mystr.length)
+console.log(mystr.indexOf("MY"))
+console.log(mystr.lastIndexOf("MY"))
+console.log(mystr.slice(2,7))
+
+let myname = "Abhay is my name";
+rep = myname.replace("Abhay","Henry");
+console.log(myname,rep)
+
+let str = "Henry am good good good boy";
+n_str = str.replace("good","bad");
+console.log(n_str)
---
+++ 8_mydate.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+
+console.log(myDate.getDay());
+console.log(myDate.getDate());
+console.log(myDate.getFullYear());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
+console.log(myDate.getMonth());
+console.log(myDate.getTime());
+console.log(myDate);
+
