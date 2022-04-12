1fe9a93dce2d071b011b225f5d4f3aec74d2d1e3
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-04-12 20:57:42 +0530

    Update JavaScript file
    Update complete for JavaScript file now just some changes remains to change the styling of HTML

--- Project/Clock/javascript.js
+++ Project/Clock/javascript.js
@@ -1,10 +1,23 @@
-//console.log("Hello world")
-var a;
-var time;
-var date;
+//console.log("Hello world");
+let a;
+let time;
+let date;
+const options = {
+   weekday: 'long',
+   year: 'numeric',
+   month: 'long',
+   day: 'numeric'
+};
+setInterval(() => {
+   a = new Date();
+   date = a.toLocaleDateString(undefined, options);
+   time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+      document.getElementById('time').innerHTML = time + '<br>on' + date;
+   //console.log(time + " on " + date);
+}, 1000);
 
-
-a = new Date();
-//date = a.toLocaleDateString();
-time = a.getHours() + ':' + a.getMinutes() + ':' a.getSeconds();
-document.getElementById('time').innerHTML = time + 'on' + a;
+/*
+setInterval(() => {
+   console.log("hello")
+},1000);
+*/
