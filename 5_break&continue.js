d6813c6699f109c4e31b87791febfcdad816fc77
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-04-16 12:20:34 +0530

    Update break&continue.js file
    Jada kuch samj nhi aaya
    really actual implementation nhi aaya 🥲
    Koi na kis or language me dekhenge 🙂

--- 5_break&continue.js
+++ 5_break&continue.js
@@ -5,3 +5,14 @@ for(var i=0;i<arr.length;i++){
     }
     console.log(arr[i]);
 }
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr);
+for(var i=0;i<arr.length;i++){
+    if(i==2){
+         break;
+         //continue;
+    }
+    console.log(arr[i])
+}
