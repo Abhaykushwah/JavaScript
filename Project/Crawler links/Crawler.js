dcf980a750454afd3725b39c54120d5e879e9364
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-04-16 12:49:21 +0530

    Project added
    Crawler in JavaScript added 😉

---
+++ Project/Crawler links/Crawler.js
@@ -0,0 +1,12 @@
+//links finder inside a webpage
+//the given  is to be search
+let str = "admin";
+let links = document.links;
+console.log(links);
+let href;
+Array.from(links).forEach(function(element) {
+  href = element.href;
+  if (href.includes(str)) {
+    console.log(href);
+  }
+});
